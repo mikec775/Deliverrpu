58492488b626fb83f1f8f58bd60aa0cc9ac9b669
ADHL <Adrian.Oah@students.ittralee.ie>
2023-10-24 16:53:14 +0100

    initialize front end

---
+++ API/DevUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IT Tralee")]
+[assembly: AssemblyProduct("DevUnitTests")]
+[assembly: AssemblyCopyright("Copyright © IT Tralee 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e782181-59f0-46cd-bad1-5b7f12ad7a8f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
