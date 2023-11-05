e465d405df54850299ad5101ddb572807021ef0b
mikec775 <61647553+mikec775@users.noreply.github.com>
2023-11-05 00:39:20 -0400

    Created ShoppingCart with additional and removal of MenuItems. Notifications included. Styling included.

--- App/deliverrpu/src/App.js
+++ App/deliverrpu/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import MenuPage from "./menuPage";
 
+import ShoppingCart from "./cart"
+import { ToastContainer } from "react-toastify";
+import "./ReactToastify.css";
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <ShoppingCart></ShoppingCart>
       <MenuPage></MenuPage>
     </div>
   );
