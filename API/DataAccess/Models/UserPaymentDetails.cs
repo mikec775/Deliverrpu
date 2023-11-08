f00502e4facf9b0780abbd72fcc821b8b12885bb
Poseidno228 <danil.t404@gmail.com>
2023-11-08 12:15:59 +0000

    Modified CardName to NameOnCard

--- API/DataAccess/Models/UserPaymentDetails.cs
+++ API/DataAccess/Models/UserPaymentDetails.cs
@@ -3,8 +3,8 @@
     public class UserPaymentDetails
     {
         public string? CardId { get; set; }
-        public int CardNumber { get; set; }
-        public string? CardName { get; set; }
+        public long CardNumber { get; set; }
+        public string? NameOnCard { get; set; }
         public string? PaymentAddress { get; set; }
         public DateTime ExpiryDate { get; set; }
         public string? UserId { get; set; }
