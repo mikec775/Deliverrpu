5d0de51409ac57a05ad9bad5693b41902d055f1d
ADHL <Adrian.Oah@students.ittralee.ie>
2023-11-08 19:29:47 +0000

    fixed add to cart toast

--- App/deliverrpu/src/components/MenuPage.js
+++ App/deliverrpu/src/components/MenuPage.js
@@ -27,7 +27,8 @@ const MenuPage = ({ setCartItems}) => {
 
   const addToCart = (item) => {
     setCartItems((curr) => [...curr, item]);
-    toast.success(`${item.name} added to cart`, {
+    console.log(item);
+    toast.success(`${item.menuName} added to cart`, {
       autoClose: 2000,
     });
   };
@@ -37,25 +38,27 @@ const MenuPage = ({ setCartItems}) => {
       <Toaster position="bottom-right" reverseOrder={false} />
       <Box sx={{ width: "100%" }}>
         <Stack spacing={1} direction={"column"}>
-          {menuItems.map((item) => (
-            <Card key={item.menuId}>
-              <CardContent>
-                <Typography variant="h4">{item.menuName}</Typography>
-                <Link to="/menuItemDetails">
-                  <Button onClick={() => console.log("Viewed")}>
-                    View Details
+          {menuItems.map((item) => {
+            return (
+              <Card key={item.menuId}>
+                <CardContent>
+                  <Typography variant="h4">{item.menuName}</Typography>
+                  <Link to="/menuItemDetails">
+                    <Button onClick={() => console.log("Viewed")}>
+                      View Details
+                    </Button>
+                  </Link>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => addToCart(item)}
+                  >
+                    Add to Cart
                   </Button>
-                </Link>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => addToCart(item)}
-                >
-                  Add to Cart
-                </Button>
-              </CardContent>
-            </Card>
-          ))}
+                </CardContent>
+              </Card>
+            )
+          })}
         </Stack>
       </Box>
     </>
