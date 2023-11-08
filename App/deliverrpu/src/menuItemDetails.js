7adabcc3e4adb6c48828f408b79f0b018ee66b67
mingxuan8833 <mingxuan8833@gmail.com>
2023-11-08 20:10:28 +0800

    menu item details page almost done, have mock data with css

--- App/deliverrpu/src/menuItemDetails.js
+++ App/deliverrpu/src/menuItemDetails.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import {Container, Box, Typography} from "@mui/material";
+import {createTheme, responsiveFontSizes, ThemeProvider} from '@mui/material/styles';
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 
 const menuItems = [
     {
@@ -29,19 +34,27 @@ function MenuItemDetails() {
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            // outlineStyle: "solid",
+            marginTop: "2%",
+            width: "20%",
+            borderRadius: "25px"
         }}>
-            <Typography variant="h6" >{menuItems[0].name}</Typography>
-            <Box><img src={menuItems[0].image} alt="no image yet"/></Box>
-            <Box>
-                 <Typography variant="body1" >{menuItems[0].description}</Typography>
-                 <Typography variant="body1" >{menuItems[0].nutrience}</Typography>
-                 <Typography variant="body1" >{menuItems[0].preptime}</Typography>
-                 <Typography variant="body1" >{menuItems[0].price}</Typography>
-            </Box>
-            <Box>
-                <Typography variant="h6"> {review[0].ReviewText} - {review[0].ReviewRating}</Typography>
-            </Box>
+            <ThemeProvider theme={theme}>
+                <Typography variant="h6" style={{padding: "5px" , outlineStyle: "solid", margin: "5%", width: "90%"}}>{menuItems[0].name}</Typography>
+                <Box><img style={{ width: "100%", height: "auto", maxWidth: "260px", maxHeight: "500px", marginTop: "2%"}} src={menuItems[0].image} alt="no image yet"/></Box>
+                <Box style={{outlineStyle: "solid", width: "90%", marginTop: "2%"}}>
+                     <Typography variant="body1" >{menuItems[0].description}</Typography>
+                     <Typography variant="body1" >{menuItems[0].nutrience}</Typography>
+                     <Typography variant="body1" >{menuItems[0].preptime}</Typography>
+                     <Typography variant="body1" >{menuItems[0].price}</Typography>
+                </Box>
+                <Box style={{outlineStyle: "solid", width: "90%", marginTop: "5%", marginBottom: "5%"}}>
+                    <Typography variant="h6"> {review[0].ReviewText}</Typography>
+                    <Typography variant="h6"> {review[0].ReviewRating} Star </Typography>
+                </Box>
+            </ThemeProvider>
         </Container>
+
     );
 }
 
